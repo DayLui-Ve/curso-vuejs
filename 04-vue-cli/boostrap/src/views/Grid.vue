66f4b62d4896f6f617c2c8a1cfebee618d28904f
Luis Briceño <luijo265@gmail.com>
2020-04-12 21:08:46 -0500

    nivel 11 video 4

---
+++ 04-vue-cli/boostrap/src/views/Grid.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="grid" class="mt-5">
+        
+        <b-row>
+            <b-col cols="12" md="6" lg="4">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel, reprehenderit ab ipsa animi tenetur corporis recusandae? Ipsa id molestias architecto sapiente ducimus vel ad, laboriosam sequi recusandae est neque. Reiciendis?</p>
+            </b-col>
+            <b-col cols="12" md="6" lg="8">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel, reprehenderit ab ipsa animi tenetur corporis recusandae? Ipsa id molestias architecto sapiente ducimus vel ad, laboriosam sequi recusandae est neque. Reiciendis?</p>
+            </b-col>
+        </b-row>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Grid',
+}
+</script>
