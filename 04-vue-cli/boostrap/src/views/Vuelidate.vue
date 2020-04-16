19e7c88abc17cebd9a6b6d1658cf049f165f82ed
Luis Briceño <luijo265@gmail.com>
2020-04-15 20:08:53 -0500

    nivel 12 video 4

--- 04-vue-cli/boostrap/src/views/Vuelidate.vue
+++ 04-vue-cli/boostrap/src/views/Vuelidate.vue
@@ -14,24 +14,46 @@
 
             <p class="text-danger" v-if="!$v.email.email">El email es incorrexto</p>
             <p class="text-danger" v-if="!$v.email.required">El email es requerido</p>
-            <p>{{$v.email}}</p>
+            <!-- <p>{{$v.email}}</p> -->
+
+            <input type="password" placeholder="Ingresa contraseña" class="form-control my-3"
+                v-model="$v.password.$model"
+                :class="{'is-invalid': $v.password.$error}"
+                >
+                <p class="text-danger" v-if="!$v.password.minLength">Mínimo 6 caracteres</p>
+                <!-- <p>{{$v.password}}</p> -->
+            <input type="password" placeholder="Repite contraseña" class="form-control my-3"
+                v-model="$v.repeatPassword.$model"
+                :class="{'is-invalid': $v.repeatPassword.$error}">
+                <p class="text-danger" v-if="!$v.repeatPassword.sameAsPassword">Contraseña incorrecta</p>
+                <!-- <p>{{$v.repeatPassword}}</p> -->
+
         </form>
 
     </div>
 </template>
 
 <script>
-import { required, email } from "vuelidate/lib/validators";
+import { required, email, sameAs, minLength } from "vuelidate/lib/validators";
 
 export default {
     name:'Vuelidate',
     data(){
         return {
             email: '',
+            password: '',
+            repeatPassword: '',
         }
     },
     validations: {
-        email: {required, email}
+        email: {required, email},
+        password: { 
+            required,
+            minLength: minLength(6),
+        },
+        repeatPassword: {
+            sameAsPassword: sameAs('password')
+        }
     }
 }
 </script>
